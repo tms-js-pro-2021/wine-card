4e89fd72325960eac646760b247898d1fedfb075
1zemzem <1zemliakova@gmail.com>
2021-08-13 12:46:57 +0300

    new commit

--- src/LandingPage/InfoSection/InfoSection.jsx
+++ src/LandingPage/InfoSection/InfoSection.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import InfoSectionWrapper from "./InfoSectionWrapper";
+import styled from "styled-components";
+import { Container } from "../../components";
+import Divider from "../../components/Divider";
+
+
+const ContainerStyled = styled(Container)`
+  background: #e6e3c4;
+  background-position: center;
+  height: 1038px;
+  padding: 80px 24px 80px 24px;
+  `;
 
 export default function InfoSection() {
   return (
-    <InfoSectionWrapper />
-  )
+    <ContainerStyled>
+      <Divider />
+      <div>hghghgh</div>
+      <div>hghghgh</div>
+      <div>hghghgh</div>
+      <Divider />
+    </ContainerStyled>
+  );
 }
