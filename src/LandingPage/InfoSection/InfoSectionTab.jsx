057a52e14bfdaffcccc930182c6bc2e0ce44f7fe
1zemzem <1zemliakova@gmail.com>
2021-08-15 10:46:13 +0300

    ready for review

--- src/LandingPage/InfoSection/InfoSectionTab.jsx
+++ src/LandingPage/InfoSection/InfoSectionTab.jsx
@@ -6,130 +6,74 @@ const InfoSectionTab = styled.table`
   padding-bottom: 30px;
 `;
 
+const InfoSectionTabColumnOne = styled.td`
+  border-style: solid;
+  border-width: 0 1px 1px 0;
+  border-spacing: 0;
+  border-color: #282828;
+  padding-right: 24px;
+`;
+
+const InfoSectionTabColumnTwo = styled.td`
+  border-style: solid;
+  border-width: 0 0 1px 0;
+  border-spacing: 0;
+  border-color: #282828;
+  padding: 10px 0 15px 30px;
+`;
+
+const InfoSectionTabColumnTwoTitle = styled.div`
+  font-family: Playfair Display;
+  font-weight: bold;
+  line-height: 150%;
+  font-size: 18px;
+  color: #282828;
+`;
+
+const InfoSectionTabColumnTwoDescription = styled.div`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 150%;
+  color: #282828;
+`;
+
 export default function InfoSectionTable() {
   return (
     <InfoSectionTab>
       <tr>
-        <td
-          style={{
-            borderStyle: "solid",
-            borderWidth: "0 1px 1px 0",
-            borderSpacing: "0",
-            borderColor: "#282828",
-            paddingRight: "24px",
-          }}
-        >
-          1980
-        </td>
-        <td
-          style={{
-            borderStyle: "solid",
-            borderWidth: "0 0 1px 0",
-            borderSpacing: "0",
-            borderColor: "#282828",
-            padding: "15px 0 15px 30px",
-          }}
-        >
-          <div
-            style={{
-              fontWeight: "bold",
-              fontSize: "18px",
-              color: "#282828",
-            }}
-          >
+        <InfoSectionTabColumnOne>1980</InfoSectionTabColumnOne>
+        <InfoSectionTabColumnTwo>
+          <InfoSectionTabColumnTwoTitle>
             Colli Euganei Bianco Ca' Lustra 1980
-          </div>
-          <div
-            style={{
-              fontSize: "14px",
-              color: "#282828",
-              paddingBottom: "15px",
-            }}
-          >
+          </InfoSectionTabColumnTwoTitle>
+          <InfoSectionTabColumnTwoDescription>
             Красочная бутылка вина из Марселя
-          </div>
-        </td>
+          </InfoSectionTabColumnTwoDescription>
+        </InfoSectionTabColumnTwo>
       </tr>
       <tr>
-        <td
-          style={{
-            borderStyle: "solid",
-            borderWidth: "0 1px 1px 0",
-            borderSpacing: "0",
-            borderColor: "#282828",
-            paddingRight: "24px",
-          }}
-        >
-          1980
-        </td>
-        <td
-          style={{
-            borderStyle: "solid",
-            borderWidth: "0 0 1px 0",
-            borderSpacing: "0",
-            borderColor: "#282828",
-            padding: "15px 0 15px 30px",
-          }}
-        >
-          <div
-            style={{
-              fontWeight: "bold",
-              fontSize: "18px",
-              color: "#282828",
-            }}
-          >
+        <InfoSectionTabColumnOne>1980</InfoSectionTabColumnOne>
+        <InfoSectionTabColumnTwo>
+          <InfoSectionTabColumnTwoTitle>
             Colli Euganei Bianco Ca' Lustra 1980
-          </div>
-          <div
-            style={{
-              fontSize: "14px",
-              color: "#282828",
-              paddingBottom: "15px",
-            }}
-          >
+          </InfoSectionTabColumnTwoTitle>
+          <InfoSectionTabColumnTwoDescription>
             Красочная бутылка вина из Марселя
-          </div>
-        </td>
+          </InfoSectionTabColumnTwoDescription>
+        </InfoSectionTabColumnTwo>
       </tr>
       <tr>
-        <td
-          style={{
-            borderStyle: "solid",
-            borderWidth: "0 1px 1px 0",
-            borderSpacing: "0",
-            borderColor: "#282828",
-            paddingRight: "24px",
-          }}
-        >
-          1980
-        </td>
-        <td
-          style={{
-            borderStyle: "solid",
-            borderWidth: "0 0 1px 0",
-            borderColor: "#282828",
-            padding: "15px 0 15px 30px",
-          }}
-        >
-          <div
-            style={{
-              fontWeight: "bold",
-              fontSize: "18px",
-              color: "#282828",
-            }}
-          >
+        <InfoSectionTabColumnOne>1980</InfoSectionTabColumnOne>
+        <InfoSectionTabColumnTwo>
+          <InfoSectionTabColumnTwoTitle>
             Colli Euganei Bianco Ca' Lustra 1980
-          </div>
-          <div
-            style={{
-              fontSize: "14px",
-              color: "#282828",
-              paddingBottom: "15px",
-            }}
-          >
+          </InfoSectionTabColumnTwoTitle>
+          <InfoSectionTabColumnTwoDescription>
             Красочная бутылка вина из Марселя
-          </div>
-        </td>
+          </InfoSectionTabColumnTwoDescription>
+        </InfoSectionTabColumnTwo>
       </tr>
     </InfoSectionTab>
   );
--- src/LandingPage/InfoSection/InfoSectionTextBlock.jsx
+++ src/LandingPage/InfoSection/InfoSectionTextBlock.jsx
@@ -7,7 +7,7 @@ const TextBlock = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  
+  padding-right: 30px;
 `;
 
 const InfoSectionLine = styled.div`
